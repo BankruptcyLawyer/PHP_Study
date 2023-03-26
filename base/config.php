01fe27de0273fe321516eeb737ad5feea9e2b712
Yaroslav <yarosl.vasil@yandex.ru>
2023-03-26 20:40:26 +0700

    третья неделя

---
+++ base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'localhost';
+const DB_NAME = 'users';
+const DB_HOST = 'root';
+const DB_PASSWORD = '';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
